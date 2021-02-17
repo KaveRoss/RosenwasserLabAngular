9db027f264473b0d956ac896c3dbdca92a6faf21
kave <43314981+KaveRoss@users.noreply.github.com>
2021-02-17 10:00:33 +0200

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss', '../assets/_scss/rosenwasserLab.scss'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'rosenwasserLab';
